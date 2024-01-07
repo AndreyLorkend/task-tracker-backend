7935e57ec8e1ce7284ac468d98003481b9054d67
AndreyLorkend <andrei190164@gmail.com>
2024-01-07 19:46:36 +0400

    feat/wip (models) расширить модель

--- models/DashboardCard.js
+++ models/DashboardCard.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const DashboardCard = new mongoose.Schema({
-  title: { type: String, required: true }
+  title: { type: String, required: true },
+  taskId: { type: String, required: true }
 })
 
 export default mongoose.model('DashboardCard', DashboardCard)

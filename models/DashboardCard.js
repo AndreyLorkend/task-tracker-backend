3c14e534212990ef002239d5bc8b33de2c5e1b9a
Andrew Derendyaev <andrei190164@gmail.com>
2024-03-24 14:40:32 +0400

    imp (models) добавить новые поля

--- models/DashboardCard.js
+++ models/DashboardCard.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const DashboardCard = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, default: "" },
-  columnId: { type: String, required: true }
+  columnId: { type: String, required: true },
+  index: { type: Number, required: false, default: 0 },
+  createdAt: { type: Number, required: false, default: new Date().getTime() },
+  updatedAt: { type: Number, required: false, default: new Date().getTime() }
 })
 
 export default mongoose.model('DashboardCard', DashboardCard)
